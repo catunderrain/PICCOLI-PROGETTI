04f2978c0b32782eb409446865eb26863b1a6e60
Thai Toan <catunderrainvn@gmail.com>
2024-07-06 09:08:40 +0700

    Done

--- DSA/vid_16/main.py
+++ DSA/vid_16/main.py
@@ -1,9 +1,24 @@
+def permute(sequence):
+    if len(sequence) == 1:
+        return [sequence]
 
+    permutations = []
 
+    for i in range(len(sequence)):
+        current = sequence[i]
+        remaining = sequence[:i] + sequence[i+1:]
 
-def A(n):
-    
-    
-    return None
+        for perm in permute(remaining):
+            permutations.append([current] + perm)
 
-A(3)
+    return permutations
+
+# Dãy số ban đầu
+sequence = [1, 2, 3,4,5]
+
+# Tạo tất cả các hoán vị
+permutations = permute(sequence)
+
+# In kết quả
+for perm in permutations:
+    print(perm)
