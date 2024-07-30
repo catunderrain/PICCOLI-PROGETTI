12868acbd3be02c81a9a5da669908c0548326e5c
Thai Toan <catunderrainvn@gmail.com>
2024-07-30 13:58:52 +0700

    NEAREST

--- PIXEL-IMAGE/main.py
+++ PIXEL-IMAGE/main.py
@@ -4,7 +4,7 @@ import os
 DIR = os.path.dirname(os.path.realpath(__file__))
 IMG = '\\' + 'guardian.jpg'
 PATH = DIR + IMG
-# PATH = r"C:\Users\Lenovo\Downloads\girlred.jpg"
+PATH = r"C:\Users\Lenovo\Downloads\download (1).jpg"
 img = cv2.imread(PATH)
 SIZE = 100
 
@@ -12,14 +12,20 @@ SIZE = 100
 def make_pixel(img, SIZE=32):
     base_shape = img.shape  
     print(base_shape)
-    # cv2.imshow(f'{base_shape}', img); cv2.waitKey(0)
 
-    r = round(1/(base_shape[1]/SIZE), 1)
-    img_mid = cv2.resize(img, (0,0), fx=r, fy=r)
-    # resize_shape = img_mid.shape; cv2.imshow(f'{resize_shape}', img_mid); cv2.waitKey(0)
-
-    img_up = cv2.resize(img_mid, (0,0), fx=4/r, fy=4/r, interpolation=cv2.INTER_AREA)
-    # cv2.imshow(f'UP {resize_shape, base_shape}', img_up); cv2.waitKey(0)
+    if base_shape[1] > base_shape[0]:
+        print(0, base_shape[0])
+        r = round(1/(base_shape[0]/SIZE), 5)
+    else:
+        print(1, base_shape[1])
+        r = round(1/(base_shape[1]/SIZE), 5)
+    print(r)
+    img_mid1 = cv2.resize(img, (0,0), fx=r, fy=r)
+    img_mid = cv2.resize(img, (0,0), fx=r, fy=r, interpolation=cv2.INTER_NEAREST)
+    
+    img_mid = np.concatenate((img_mid1, img_mid), 1)
+    # img_up = cv2.resize(img_mid, (0,0), fx=1/r, fy=1/r, interpolation=cv2.INTER_NEAREST)
+    img_up = cv2.resize(img_mid, (0,0), fx=1/r, fy=1/r, interpolation=cv2.INTER_AREA)
     
     return img_up
 
---
+++ PIXEL-IMAGE/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+    img_mid1 = cv2.resize(img, (0,0), fx=r, fy=r)
